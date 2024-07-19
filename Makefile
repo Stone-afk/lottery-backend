c5551cfd0c75b765f6b95a44b65cac2d5fe218fd
Stone-afk <1711865140@qq.com>
2024-07-19 23:39:38 +0800

    feat: add docker config

---
+++ Makefile
@@ -0,0 +1,4 @@
+# e2e 测试
+.PHONY: e2e
+e2e:
+	sh ./deploy/script/integrate_test.sh
---
+++ deploy/sql/looklook_usercenter.sql
@@ -0,0 +1,163 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : gozero-looklook
+ Source Server Type    : MySQL
+ Source Server Version : 80028
+ Source Host           : 127.0.0.1:33069
+ Source Schema         : looklook_usercenter
+
+ Target Server Type    : MySQL
+ Target Server Version : 80028
+ File Encoding         : 65001
+
+ Date: 18/01/2024 21:16:45
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `delete_time` datetime(0) DEFAULT NULL,
+  `del_state` tinyint(0) NOT NULL DEFAULT 0,
+  `version` bigint(0) NOT NULL DEFAULT 0 COMMENT '版本号',
+  `mobile` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `sex` tinyint(1) NOT NULL DEFAULT 0 COMMENT '性别 0:男 1:女',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '头像',
+  `info` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `is_admin` tinyint(1) DEFAULT 0 COMMENT '是否管理员 1是 0否',
+  `signature` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '个性签名',
+  `location_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '地址名称',
+  `longitude` DOUBLE PRECISION NOT NULL DEFAULT 0 COMMENT '经度',
+  `latitude` DOUBLE PRECISION NOT NULL DEFAULT 0 COMMENT '纬度',
+  `total_prize` int(0) NOT NULL DEFAULT 0 COMMENT '累计奖品',
+  `fans` int(0) NOT NULL DEFAULT 0 COMMENT '粉丝数量',
+  `all_lottery` int(0) NOT NULL DEFAULT 0 COMMENT '全部抽奖包含我发起的、我中奖的',
+  `initiation_record` int(0) NOT NULL DEFAULT 0 COMMENT '发起抽奖记录',
+  `winning_record` int(0) NOT NULL DEFAULT 0 COMMENT '中奖记录',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_mobile`(`mobile`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `user_auth`;
+CREATE TABLE `user_auth`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `delete_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `del_state` tinyint(0) NOT NULL DEFAULT 0,
+  `version` bigint(0) NOT NULL DEFAULT 0 COMMENT '版本号',
+  `user_id` bigint(0) NOT NULL DEFAULT 0,
+  `auth_key` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '平台唯一id',
+  `auth_type` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '平台类型',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_type_key`(`auth_type`, `auth_key`) USING BTREE,
+  UNIQUE INDEX `idx_userId_key`(`user_id`, `auth_type`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户授权表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user_sponsor
+-- ----------------------------
+DROP TABLE IF EXISTS `user_sponsor`;
+CREATE TABLE `user_sponsor`  (
+ `id` int(0) NOT NULL AUTO_INCREMENT,
+ `user_id` int(0) NOT NULL DEFAULT 0,
+ `type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1微信号 2公众号 3小程序 4微信群 5视频号',
+ `applet_type` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'type=3时该字段才有意义，1小程序链接 2路径跳转 3二维码跳转',
+ `name` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '名称',
+ `desc` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '描述',
+ `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '头像',
+ `is_show` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1显示 2不显示',
+ `qr_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '二维码图片地址, type=1 2 3&applet_type=3 4的时候启用',
+ `input_a` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT 'type=5 applet_type=2 or applet_type=1 输入框A',
+ `input_b` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT 'type=5 applet_type=2输入框B',
+ `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+ `delete_time` datetime(0) DEFAULT NULL,
+ PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '抽奖发起人联系方式（抽奖赞助商）' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------
+-- Table structure for user_shop
+-- ----------------------------
+DROP TABLE IF EXISTS `user_shop`;
+CREATE TABLE `user_shop`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `user_id` int(0) NOT NULL DEFAULT 0,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `location` decimal(65, 0) NOT NULL DEFAULT 0,
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `delete_time` datetime(0) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+--
+-- Table structure for table `user_address`
+--
+DROP TABLE IF EXISTS `user_address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_address` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL DEFAULT '0' COMMENT '用户id',
+  `contact_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '联系人姓名',
+  `contact_mobile` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '联系人手机号码',
+  `district` json NOT NULL COMMENT '地区信息',
+  `detail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '详细地址',
+  `postcode` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '邮政编码',
+  `is_default` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否为默认地址 1是 0否',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='用户收货地址表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- ----------------------------
+-- Table structure for user_contact
+-- ----------------------------
+DROP TABLE IF EXISTS `user_contact`;
+CREATE TABLE `user_contact`  (
+                                 `id` int(0) NOT NULL AUTO_INCREMENT,
+                                 `user_id` int(0) NOT NULL DEFAULT 0,
+                                 `content` json NOT NULL COMMENT 'content',
+                                 `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT 'remark',
+                                 `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                 `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+                                 PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '抽奖发起人联系方式' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------
+-- Table structure for user_dynamic
+-- ----------------------------
+DROP TABLE IF EXISTS `user_dynamic`;
+CREATE TABLE `user_dynamic`  (
+     `id` int(0) NOT NULL AUTO_INCREMENT,
+     `user_id` int not null comment '发布动态用户id',
+     `dynamic_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci not null comment '发布动态地址',
+     `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '动态描述',
+     `create_time` datetime not null default current_timestamp,
+     `update_time` datetime not null default current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+     `delete_time` datetime DEFAULT NULL,
+     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户发布动态表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;

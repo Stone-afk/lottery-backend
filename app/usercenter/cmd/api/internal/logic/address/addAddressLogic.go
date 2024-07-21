c0cb340260d40b5cad49c3a24d7e8eba9cfd60d4
Stone-afk <1711865140@qq.com>
2024-07-21 23:00:04 +0800

    add rpc

--- app/usercenter/cmd/api/internal/logic/address/addAddressLogic.go
+++ app/usercenter/cmd/api/internal/logic/address/addAddressLogic.go
@@ -2,11 +2,15 @@ package address
 
 import (
 	"context"
-
+	"encoding/json"
+	"github.com/jinzhu/copier"
+	"github.com/pkg/errors"
+	"github.com/zeromicro/go-zero/core/logx"
 	"looklook/app/usercenter/cmd/api/internal/svc"
 	"looklook/app/usercenter/cmd/api/internal/types"
-
-	"github.com/zeromicro/go-zero/core/logx"
+	"looklook/app/usercenter/cmd/rpc/pb"
+	"looklook/common/ctxdata"
+	"looklook/common/xerr"
 )
 
 type AddAddressLogic struct {
@@ -24,7 +28,17 @@ func NewAddAddressLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddAdd
 }
 
 func (l *AddAddressLogic) AddAddress(req *types.AddAddressReq) (resp *types.AddAddressResp, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+	pbAddressReq := new(pb.AddUserAddressReq)
+	err = copier.Copy(pbAddressReq, req)
+	pbAddressReq.UserId = ctxdata.GetUidFromCtx(l.ctx)
+	districtByte, err := json.Marshal(req.District)
+	if err != nil {
+		return nil, err
+	}
+	pbAddressReq.District = string(districtByte)
+	addAddress, err := l.svcCtx.UsercenterRpc.AddUserAddress(l.ctx, pbAddressReq)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewErrMsg("add address fail"), "add address rpc AddUserAddress fail req: %+v , err : %v ", req, err)
+	}
+	return &types.AddAddressResp{Id: addAddress.Id}, nil
 }

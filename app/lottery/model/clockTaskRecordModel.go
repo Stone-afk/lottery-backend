da4470f7ed6040c5e44d707dd1d36819a131756e
Stone-afk <1711865140@qq.com>
2024-07-28 19:55:02 +0800

    feat: add lettry

--- app/lottery/model/clockTaskRecordModel.go
+++ app/lottery/model/clockTaskRecordModel.go
@@ -12,12 +12,20 @@ type (
 	// and implement the added methods in customClockTaskRecordModel.
 	ClockTaskRecordModel interface {
 		clockTaskRecordModel
+		// GetClockTaskRecordByLotteryIdAndUserIds 自定义方法
+		// 传入lotteryId以及userIds，得到每个参与者的中奖倍率
+		GetClockTaskRecordByLotteryIdAndUserIds(lotteryId int64, userIds []int64) ([]*ClockTaskRecord, error)
 	}
 
 	customClockTaskRecordModel struct {
 		*defaultClockTaskRecordModel
 	}
 )
+
+func (c *customClockTaskRecordModel) GetClockTaskRecordByLotteryIdAndUserIds(lotteryId int64, userIds []int64) ([]*ClockTaskRecord, error) {
+	//TODO implement me
+	panic("implement me")
+}
 
 // NewClockTaskRecordModel returns a model for the database table.
 func NewClockTaskRecordModel(conn sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) ClockTaskRecordModel {

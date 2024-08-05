d9f971af08ce861f27a9d0c3a880494cbfe609c5
Stone-afk <1711865140@qq.com>
2024-08-05 13:55:50 +0800

    feat: add comment

--- app/comment/cmd/api/internal/config/config.go
+++ app/comment/cmd/api/internal/config/config.go
@@ -1,11 +1,22 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
-	rest.RestConf
 	JwtAuth struct {
 		AccessSecret string
 		AccessExpire int64
 	}
+	DB struct {
+		DataSource string
+	}
+	rest.RestConf
+	Cache             cache.CacheConf
+	CommentRpcConf    zrpc.RpcClientConf
+	UserCenterRpcConf zrpc.RpcClientConf
+	LotteryRpcConf    zrpc.RpcClientConf
 }

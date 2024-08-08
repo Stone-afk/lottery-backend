c0d3b401c714be34dc875265c17cd315513f3966
Stone-afk <1711865140@qq.com>
2024-08-08 11:22:27 +0800

    feat: add checkin

--- app/checkin/model/integralModel.go
+++ app/checkin/model/integralModel.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"context"
+	"database/sql"
 	"github.com/zeromicro/go-zero/core/stores/cache"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
@@ -12,12 +14,36 @@ type (
 	// and implement the added methods in customIntegralModel.
 	IntegralModel interface {
 		integralModel
+		FindOneByUserId(ctx context.Context, userId int64) (*Integral, error)
+		TransUpdateByUserId(ctx context.Context, session sqlx.Session, data *Integral) error
+		TransInsertByUserId(ctx context.Context, session sqlx.Session, data *Integral) (sql.Result, error)
+		UpdateByUserId(ctx context.Context, data *Integral) error
 	}
 
 	customIntegralModel struct {
 		*defaultIntegralModel
 	}
 )
+
+func (m *defaultIntegralModel) FindOneByUserId(ctx context.Context, userId int64) (*Integral, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *defaultIntegralModel) TransUpdateByUserId(ctx context.Context, session sqlx.Session, data *Integral) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *defaultIntegralModel) TransInsertByUserId(ctx context.Context, session sqlx.Session, data *Integral) (sql.Result, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *defaultIntegralModel) UpdateByUserId(ctx context.Context, data *Integral) error {
+	//TODO implement me
+	panic("implement me")
+}
 
 // NewIntegralModel returns a model for the database table.
 func NewIntegralModel(conn sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) IntegralModel {
--- app/checkin/model/taskRecordModel.go
+++ app/checkin/model/taskRecordModel.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"context"
+	"github.com/Masterminds/squirrel"
 	"github.com/zeromicro/go-zero/core/stores/cache"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
@@ -12,12 +14,36 @@ type (
 	// and implement the added methods in customTaskRecordModel.
 	TaskRecordModel interface {
 		taskRecordModel
+		FindByUserId(ctx context.Context, userId int64, builder squirrel.SelectBuilder, orderBy string) ([]*TaskRecord, error)
+		FindByUserIdAndTaskId(ctx context.Context, userId int64, taskId int64) (*TaskRecord, error)
+		FindByUserIdAndTaskIdByDay(ctx context.Context, userId int64, taskId int64) (*TaskRecord, error)
+		FindByUserIdAndTaskIdByWeek(ctx context.Context, userId int64, taskId int64) (*TaskRecord, error)
 	}
 
 	customTaskRecordModel struct {
 		*defaultTaskRecordModel
 	}
 )
+
+func (m *defaultTaskRecordModel) FindByUserId(ctx context.Context, userId int64, builder squirrel.SelectBuilder, orderBy string) ([]*TaskRecord, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *defaultTaskRecordModel) FindByUserIdAndTaskId(ctx context.Context, userId int64, taskId int64) (*TaskRecord, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *defaultTaskRecordModel) FindByUserIdAndTaskIdByDay(ctx context.Context, userId int64, taskId int64) (*TaskRecord, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *defaultTaskRecordModel) FindByUserIdAndTaskIdByWeek(ctx context.Context, userId int64, taskId int64) (*TaskRecord, error) {
+	//TODO implement me
+	panic("implement me")
+}
 
 // NewTaskRecordModel returns a model for the database table.
 func NewTaskRecordModel(conn sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) TaskRecordModel {
